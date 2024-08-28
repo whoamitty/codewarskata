6404f484488fc1e6613df7bab857b04544468711
whoamitty <74999206+whoamitty@users.noreply.github.com>
2024-08-28 16:53:09 -0300

    new exo from frend not codewars, and better code

--- exo3.py
+++ exo3.py
@@ -2,20 +2,21 @@
 
 # https://www.codewars.com/kata/55edaba99da3a9c84000003b/train/python
 
+# def divisible_by(numbers, divisor):
+#     output=[itemnum for itemnum in numbers if (numbers/divisor)%divisor ]
+#     return output
+
+
+
+
+
 def divisible_by(numbers, divisor):
-    output=[itemnum for itemnum in numbers if divisible(itemnum, divisor)]
+    output=[itemnum for itemnum in numbers if  itemnum%divisor==0 ]
+    #output=[itemnum for itemnum in numbers if not itemnum%divisor ] #work too
+
+    print(output)
     return output
 
 
-
-def divisible(itemnum, divisor):
-   
-   result=itemnum/divisor
-   if not (result).is_integer():
-
-    return False
-   else:
-    return True
-
-
+divisible_by([1, 2, 3, 4, 5, 6], 2)
 
---
+++ exo4.py
@@ -0,0 +1,7 @@
+# not codewars
+
+numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(numbers)
+print([itemnum for itemnum in numbers if (itemnum/2).is_integer()])
+
+
